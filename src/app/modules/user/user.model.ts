a8f35b5cc85e10f37889bef12a47d4b10c78f19d
pobi <pobittrachandraroy2001@gmail.com>
2024-06-12 21:13:43 +0600

    implement interface and model of user

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,27 @@
+import { Schema } from "mongoose";
+import { TUser } from "./user.interface";
+
+const userSchema = new Schema<TUser>({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
