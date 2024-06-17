35cb993754a3331b6aec16ef5d6c938089124775
pobi <pobittrachandraroy2001@gmail.com>
2024-06-17 21:19:48 +0600

    implement about not Found route. create not found route handler in middleware and excute it from app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
+import notFoundRoute from "./app/middlewares/notFoundRoute";
 const app: Application = express();
 
 //middlewares
@@ -10,4 +11,6 @@ app.use(cors());
 //main application route
 app.use("/api", router);
 
+//not found route
+app.use(notFoundRoute);
 export default app;
